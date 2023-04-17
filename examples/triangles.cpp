6189b755bd1d0c71bab54e91142f74dad0669042
Hendrik Steenberg <hen3.steenberg@gmail.com>
2023-04-17 13:46:11 +0200

    updated triangles.cpp to draw a moving triangle.

--- examples/triangles.cpp
+++ examples/triangles.cpp
@@ -2,6 +2,7 @@
 #include "obscure/configuration/default_configuration.h"
 #include <fstream>
 #include <iostream>
+#include <cmath>
 
 
 struct triangles_app : obscure::application_2d
@@ -11,9 +12,9 @@ struct triangles_app : obscure::application_2d
 
 	std::array<obscure::shape::colored_vertex, 3> vertices = 
 	{
-		obscure::shape::colored_vertex({0.0f, -0.5f}, {1.0f, 1.0f, 1.0f}),
-		obscure::shape::colored_vertex({0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}),
-		obscure::shape::colored_vertex({-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f})
+		obscure::shape::colored_vertex({0.0f, -0.1f}, {1.0f, 1.0f, 1.0f}),
+		obscure::shape::colored_vertex({0.1f, 0.1f}, {0.0f, 1.0f, 0.0f}),
+		obscure::shape::colored_vertex({-0.1f, 0.1f}, {0.0f, 0.0f, 1.0f})
 	};
 
 	triangles_app()
@@ -24,6 +25,18 @@ struct triangles_app : obscure::application_2d
 
 	void loop(obscure::vulkan::command_sequence draw_context)
 	{
+		static obscure::stopwatch frame_timer;
+		auto elapsed_time = frame_timer.elapsed_time();
+		float cos = std::cos(elapsed_time.count());
+
+		float top = -0.12f * cos - 0.1f;
+		vertices[0].vertex.y = top;
+
+		float bottom = -0.1f * cos + 0.1f;
+		vertices[1].vertex.y = bottom;
+		vertices[2].vertex.y = bottom;
+
+
 		draw_context
 			.begin_rendering()
 			.draw_colored_triangle_list(vertices, vertex_buffer)
