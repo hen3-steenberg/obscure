f3eece3e939cd070e149dbb87e59827e1ae9ec35
hen3-steenberg <hen3.steenberg@gmail.com>
2024-05-09 15:40:19 +0200

    fixed json logger formatting

--- obscure/include/obscure/vulkan/logger.hpp
+++ obscure/include/obscure/vulkan/logger.hpp
@@ -1,5 +1,3 @@
-#include <bit>
-#include <utility>
 #ifndef OBSCURE_VULKAN_LOGGER_DEFINITION
 #define OBSCURE_VULKAN_LOGGER_DEFINITION 1
 #include "glfw_vulkan_include.hpp"
@@ -8,7 +6,17 @@
 #include <iostream>
 #include <fstream>
 #include <atomic>
-
+#include <vector>
+#include <cstdint>
+#include <utility>
+#include <string>
+#include <array>
+#include <queue>
+#include <chrono>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <thread>
 
 namespace obscure
 {
@@ -26,7 +34,7 @@ namespace obscure
                 if(pUserData)
                 {
                     logger_base* logger = reinterpret_cast<logger_base*>(pUserData);
-                    logger->LogEvent(messageSeverity, messageType, pCallbackData);
+                    logger->LogEvent(messageSeverity, messageType, *pCallbackData);
                 }
                 return VK_FALSE;
             }
@@ -55,12 +63,12 @@ namespace obscure
 
             protected:
 
-            virtual void LogEvent(VkDebugUtilsMessageSeverityFlagBitsEXT Severity, VkDebugUtilsMessageTypeFlagsEXT Type, const VkDebugUtilsMessengerCallbackDataEXT* Data) = 0;
+            virtual void LogEvent(VkDebugUtilsMessageSeverityFlagsEXT Severity, VkDebugUtilsMessageTypeFlagsEXT Type, const VkDebugUtilsMessengerCallbackDataEXT & Data) = 0;
 
 
         };
 
-        constexpr const char* get_message_severity_text(VkDebugUtilsMessageSeverityFlagBitsEXT Severity) noexcept
+        constexpr const char* get_message_severity_text(VkDebugUtilsMessageSeverityFlagsEXT Severity) noexcept
         {
             using namespace obscure::helper_templates;
             constexpr auto verbose = max_set_bits(VkDebugUtilsMessageSeverityFlagBitsEXT::VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT);
@@ -98,13 +106,121 @@ namespace obscure
             }
         }
 
+        struct Event
+        {
+            VkDebugUtilsMessageSeverityFlagsEXT Severity;
+            VkDebugUtilsMessageTypeFlagsEXT Type;
+            struct LogData
+            {
+
+                struct Label
+                {
+                    std::string name;
+                    std::array<float, 4> color;
+
+                    Label(vk::DebugUtilsLabelEXT const& lbl)
+                        : name(lbl.pLabelName ? lbl.pLabelName : "NO NAME"), color(lbl.color)
+                    {}
+                };
+
+                struct Object
+                {
+                    vk::ObjectType Type;
+                    uint64_t Handle;
+                    std::string name;
+
+                    Object(vk::DebugUtilsObjectNameInfoEXT const& obj)
+                        : Type(obj.objectType), Handle(obj.objectHandle), name(obj.pObjectName ? obj.pObjectName : "NO NAME")
+                    {}
+                };
+
+                int32_t ID;
+                std::string IDName;
+                std::string Message;
+
+                std::vector<Label> queue_labels;
+                std::vector<Label> cmd_buffer_labels;
+
+                std::vector<Object> objects;
+
+                LogData(const VkDebugUtilsMessengerCallbackDataEXT & Data)
+                    :   ID(Data.messageIdNumber),
+                        IDName(Data.pMessageIdName ? Data.pMessageIdName : "NO NAME"),
+                        Message(Data.pMessage ? Data.pMessage : "NO MESSAGE"),
+                        queue_labels(),
+                        cmd_buffer_labels(),
+                        objects()
+                {
+                    queue_labels.reserve(Data.queueLabelCount);
+                    for(size_t index = 0; index < Data.queueLabelCount; ++index)
+                    {
+                        queue_labels.emplace_back(Data.pQueueLabels[index]);
+                    }
+
+                    cmd_buffer_labels.reserve(Data.cmdBufLabelCount);
+                    for(size_t index = 0; index < Data.cmdBufLabelCount; ++index)
+                    {
+                        cmd_buffer_labels.emplace_back(Data.pCmdBufLabels[index]);
+                    }
+
+                    objects.reserve(Data.objectCount);
+                    for(size_t index = 0; index < Data.objectCount; ++index)
+                    {
+                        objects.emplace_back(Data.pObjects[index]);
+                    }
+
+                    for(auto & c : Message)
+                    {
+                        if(c == '"')
+                        {
+                            c = '\'';
+                        }
+                    }
+                }
+            } data;
+            Event(VkDebugUtilsMessageSeverityFlagsEXT _Severity, VkDebugUtilsMessageTypeFlagsEXT _Type, const VkDebugUtilsMessengerCallbackDataEXT & _Data)
+                : Severity(_Severity), Type(_Type), data(_Data)
+            {
+
+            }
+        };
+
+        struct EventQueue
+        {
+            std::queue<std::unique_ptr<Event>> events;
+            std::mutex queue_mtx;
+
+            void enqueue(VkDebugUtilsMessageSeverityFlagsEXT Severity, VkDebugUtilsMessageTypeFlagsEXT Type, const VkDebugUtilsMessengerCallbackDataEXT & Data)
+            {
+                std::lock_guard queue_guard{queue_mtx};
+                events.push(std::make_unique<Event>(Severity, Type, Data));
+            }
+
+            bool empty() const
+            {
+                return events.empty();
+            }
+
+            std::optional<std::unique_ptr<Event>> get_next_event()
+            {
+                std::lock_guard queue_guard{queue_mtx};
+                if(events.empty()) return std::nullopt;
+                else
+                {
+                    std::unique_ptr<Event> result = std::move(events.front());
+                    events.pop();
+                    return result;
+                };
+            }
+        };
+
         template<typename ... TSiblings>
         struct verbose_console_logger : logger_base<TSiblings ...>
         {
             private:
-            void LogEvent(VkDebugUtilsMessageSeverityFlagBitsEXT Severity, VkDebugUtilsMessageTypeFlagsEXT Type, const VkDebugUtilsMessengerCallbackDataEXT* Data) final
+            void LogEvent(VkDebugUtilsMessageSeverityFlagsEXT Severity, VkDebugUtilsMessageTypeFlagsEXT Type, const VkDebugUtilsMessengerCallbackDataEXT & Data) final
             {
-                std::cout << get_message_severity_text(Severity) << ", " << get_message_type_text(Type) << " : " << Data->pMessage << "\n";
+                std::cout << get_message_severity_text(Severity) << ", " << get_message_type_text(Type) << " : " << Data.pMessage << "\n";
             }
         };
 
@@ -112,9 +228,13 @@ namespace obscure
         struct verbose_json_logger : logger_base<TSiblings ...>
         {
             std::ofstream json_file;
-            std::atomic_flag is_busy = ATOMIC_FLAG_INIT;
+            std::atomic_flag request_stop = ATOMIC_FLAG_INIT;
+            EventQueue event_queue;
+            std::thread writer_thread;
+            bool first_entry = true;
+            
             verbose_json_logger()
-                : json_file("vulkan_log.json")
+                : json_file("vulkan_log.json"), writer_thread([this](){ ProcessMessages(); })
             {
                 json_file << "[\n";
             }
@@ -122,55 +242,114 @@ namespace obscure
 
             ~verbose_json_logger()
             {
-                is_busy.wait(true);
+                request_stop.test_and_set();
+                writer_thread.join();
                 json_file << "\n]";
                 json_file.flush();
                 json_file.close();
             }
 
             private:
-            void LogEvent(VkDebugUtilsMessageSeverityFlagBitsEXT Severity, VkDebugUtilsMessageTypeFlagsEXT Type, const VkDebugUtilsMessengerCallbackDataEXT* Data) final
+
+            void ProcessMessages()
             {
-                is_busy.test_and_set();
-                json_file << "{\n";
-                json_file << "\t\"severity\" : \"" << get_message_severity_text(Severity) << "\",\n";
-                json_file << "\t\"type\" : \"" << get_message_type_text(Type) << "\",\n";
-                json_file << "\t\"id\" : " << Data->messageIdNumber << ",\n";
-                json_file << "\t\"id_name\" : \"" << Data->pMessageIdName << "\",\n";
-                json_file << "\t\"message\" : \"" << Data->pMessage << "\",\n";
-                json_file << "\t\"queue_labels\" : [\n";
-                for(size_t label_idx = 0; label_idx < Data->queueLabelCount; ++label_idx)
+                using namespace std::literals;
+                while(!request_stop.test() || !event_queue.empty())
                 {
-                    const auto& label = Data->pQueueLabels[label_idx];
-                    json_file << "\t{\n";
-                    json_file << "\t\t\"name\" : \"" << label.pLabelName << "\",\n";
+                    auto event = event_queue.get_next_event();
+                    if(event.has_value())
+                    {
+                        HandleLogEntry(std::move(event.value()));
+                    }
+                    else 
+                    {
+                        std::this_thread::sleep_for(100ms);
+                    }
+                }
+            }
+
+            void HandleLogEntry(std::unique_ptr<Event> Entry)
+            {
+                if(first_entry)
+                {
+                    json_file << "{\n";
+                    first_entry = false;
+                }
+                else {
+                    json_file << ",{\n";
+                }
+                
+                json_file << "\t\"severity\" : \"" << get_message_severity_text(Entry->Severity) << "\",\n";
+                json_file << "\t\"type\" : \"" << get_message_type_text(Entry->Type) << "\",\n";
+                json_file << "\t\"id\" : " << Entry->data.ID << ",\n";
+                json_file << "\t\"id_name\" : \"" << Entry->data.IDName << "\",\n";
+                json_file << "\t\"message\" : \"" << Entry->data.Message << "\",\n";
+                json_file << "\t\"queue_labels\" : [\n";
+
+                bool first = true;
+
+                for(auto const& label : Entry->data.queue_labels)
+                {
+                    if(first)
+                    {
+                        json_file << "\t{\n";
+                        first = false;
+                    }
+                    else 
+                    {
+                        json_file << "\t,{\n";
+                    }
+                    json_file << "\t\t\"name\" : \"" << label.name << "\",\n";
                     json_file << "\t\t\"color\" : [" << label.color[0] << ", " << label.color[1] << ", " << label.color[2] << ", " << label.color[3] << "]\n";
-                    json_file << "\t},\n";
+                    json_file << "\t}\n";
                 }
                 json_file << "\t],\n";
                 json_file << "\t\"command_buffer_labels\" : [\n";
-                for(size_t label_idx = 0; label_idx < Data->cmdBufLabelCount; ++label_idx)
+
+                first = true;
+
+                for(auto const& label : Entry->data.cmd_buffer_labels)
                 {
-                    const auto& label = Data->pCmdBufLabels[label_idx];
-                    json_file << "\t{\n";
-                    json_file << "\t\t\"name\" : \"" << label.pLabelName << "\",\n";
+                    if(first)
+                    {
+                        json_file << "\t{\n";
+                        first = false;
+                    }
+                    else 
+                    {
+                        json_file << "\t,{\n";
+                    }
+                    json_file << "\t\t\"name\" : \"" << label.name << "\",\n";
                     json_file << "\t\t\"color\" : [" << label.color[0] << ", " << label.color[1] << ", " << label.color[2] << ", " << label.color[3] << "]\n";
-                    json_file << "\t},\n";
+                    json_file << "\t}\n";
                 }
                 json_file << "\t],\n";
                 json_file << "\t\"objects\" : [\n";
-                for(size_t obj_idx = 0; obj_idx < Data->objectCount; ++obj_idx)
+
+                first = true;
+                for(auto const& obj : Entry->data.objects)
                 {
-                    const auto& obj = Data->pObjects[obj_idx];
-                    json_file << "\t{\n";
-                    json_file << "\t\t\"type\" : " << obj.objectType << ",\n";
-                    json_file << "\t\t\"handle\" : " << obj.objectHandle << ",\n";
-                    json_file << "\t\t\"name\" : \"" << obj.pObjectName << "\"\n";
-                    json_file << "\t},\n";
+                    if(first)
+                    {
+                        json_file << "\t{\n";
+                        first = false;
+                    }
+                    else 
+                    {
+                        json_file << "\t,{\n";
+                    }
+                    json_file << "\t\t\"type\" : " << (size_t)obj.Type << ",\n";
+                    json_file << "\t\t\"handle\" : " << obj.Handle << ",\n";
+                    json_file << "\t\t\"name\" : \"" << obj.name << "\"\n";
+                    json_file << "\t}\n";
                 }
                 json_file << "\t]\n";
-                json_file << "},\n";
-                is_busy.clear();
+                json_file << "}\n";
+            }
+
+            void LogEvent(VkDebugUtilsMessageSeverityFlagsEXT Severity, VkDebugUtilsMessageTypeFlagsEXT Type, const VkDebugUtilsMessengerCallbackDataEXT& Data) final
+            {
+                event_queue.enqueue(Severity, Type, Data);
             }
         };
 
