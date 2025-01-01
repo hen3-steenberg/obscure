2313559fd1335370489b3bb510cd98803f1f4047
Hendri Steenberg <hen3.steenberg@gmail.com>
2025-01-01 09:51:42 +0200

    Added the `test_triangle` example

---
+++ examples/test_triangle.cpp
@@ -0,0 +1,30 @@
+#include "obscure/vulkan/builtin_pipelines/test_triangle.hpp"
+#include "obscure/application.hpp"
+
+
+int main()
+{
+
+    obscure::application<obscure::vulkan::builtin_pipelines::test_triangle> app{"Test App", obscure::version{1,0,0}};
+
+    {
+
+        while (!app.window.should_close())
+        {
+            glfwPollEvents();
+            if (app.window.isKeyPressed(GLFW_KEY_ESCAPE))
+            {
+                break;
+            }
+            {
+                auto frame_ctx = app.begin_frame();
+                frame_ctx.draw_test_triangle();
+            }
+            app.submit_frame();
+            app.draw_frame();
+        }
+    }
+
+
+    return 0;
+}
