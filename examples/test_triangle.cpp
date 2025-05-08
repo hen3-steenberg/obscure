3fc542b0235cb66d4e9ad1a18bc85c39bd67daa4
hen3 <hendri.steenberg@protonmail.com>
2025-05-08 15:18:01 +0200

    Fixed instance lifetime issue. Vulkan instance should be freed before main exits.

--- examples/test_triangle.cpp
+++ examples/test_triangle.cpp
@@ -6,22 +6,26 @@ int main()
 {
     obscure::initialize("Triangle Test App", obscure::version{1,0,0});
 
-    obscure::graphics_context<obscure::vulkan::builtin_pipelines::test_triangle> app{};
-
-    while (!app.window.should_close())
     {
-        glfwPollEvents();
-        if (app.window.isKeyPressed(GLFW_KEY_ESCAPE))
+        obscure::graphics_context<obscure::vulkan::builtin_pipelines::test_triangle> app{};
+
+        while (!app.window.should_close())
         {
-            break;
+            glfwPollEvents();
+            if (app.window.isKeyPressed(GLFW_KEY_ESCAPE))
+            {
+                break;
+            }
+            {
+                auto frame_ctx = app.begin_frame();
+                frame_ctx.draw_test_triangle();
+            }
+            app.submit_frame();
+            app.draw_frame();
         }
-        {
-            auto frame_ctx = app.begin_frame();
-            frame_ctx.draw_test_triangle();
-        }
-        app.submit_frame();
-        app.draw_frame();
     }
+
+    obscure::free_instance();
 
     return 0;
 }
